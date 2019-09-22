10d5a352e6365eff6fb54558be370fbbeb01b17a
johnny <nintendof1@gmail.com>
2019-09-22 12:41:30 +0800

    react-app

--- NODE_SERVER/server.js
+++ NODE_SERVER/server.js
@@ -1,11 +1,14 @@
 const express = require('express');
 
 // Constants
-const PORT = 3000;
+const PORT = 5000;
 // App
 const app = express();
 // 提供靜態檔案顯示
 app.use('/html', express.static(__dirname + '/html'));
+
+// 提供React build 檔案顯示
+app.use('/react', express.static(__dirname + '/react_app_build'));
 
 app.get('/', (req, res) => {
     res.send('Hello world ,\nNode.js + Express.js\nHTML靜態網頁');
