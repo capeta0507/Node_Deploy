3811fbdfbfc8b605fd9f31ead9bd71fd1738126d
詹政勳 <nintendof1@gmail.com>
2019-09-26 22:15:40 +0800

    next

--- next_express/pages/index.js
+++ next_express/pages/index.js
@@ -1,87 +1,101 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Head from 'next/head'
-import Nav from '../components/nav'
+import '../style/index.css'
 
-const Home = () => (
-  <div>
-    <Head>
-      <title>Home</title>
-    </Head>
+const Home = () => {
+  const [f1data] = useState([
+    {
+      name: 'Mclaren Mercedes',
+      img: 'Mercedes-Benz-McLaren-MP4-15-14.jpg',
+      title: 'MP4-15'
+    },
+    {
+      name: 'Mika Hakkinen',
+      img: 'mika-hakkinen.jpg',
+      title: 'Lotus -> Mclaren'
+    },
+    {
+      name: 'David Coulthard',
+      img: 'david-coulthard-portrait.jpg',
+      title: 'Williams -> Mclaren -> Red Bull'
+    }
+  ])
+  return(
+    <div>
+      <Head>
+        <title>Home</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" />
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+      </Head>
 
-    <Nav />
-
-    <div className='hero'>
-      <h1 className='title'>Welcome to Next.js!</h1>
-      <p className='description'>
-        To get started, edit <code>pages/index.js</code> and save to reload.
-      </p>
-
-      <div className='row'>
-        <a href='https://nextjs.org/docs' className='card'>
-          <h3>Documentation &rarr;</h3>
-          <p>Learn more about Next.js in the documentation.</p>
-        </a>
-        <a href='https://nextjs.org/learn' className='card'>
-          <h3>Next.js Learn &rarr;</h3>
-          <p>Learn about Next.js by following an interactive tutorial!</p>
-        </a>
-        <a
-          href='https://github.com/zeit/next.js/tree/master/examples'
-          className='card'
-        >
-          <h3>Examples &rarr;</h3>
-          <p>Find other example boilerplates on the Next.js GitHub.</p>
-        </a>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <div className="container">
+          <a className="navbar-brand" href="#">
+            <img className="logo" src="https://www.thedesignfrontier.com/wp-content/uploads/2019/05/f1-logo-big.png" alt="" />
+          </a>
+          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+        
+          <div className="collapse navbar-collapse" id="navbarColor01">
+            <ul className="navbar-nav mr-auto">
+              <li className="nav-item active">
+                <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">Drivers</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">Teams</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+      <div className="container">
+        <div className="title">
+          <h1>West McLaren Mercedes</h1>
+        </div>
+        <div id='myCard' className="row">
+          {f1data.map((data,idx) => {
+            let photo = data.img
+            console.log(data)
+            return (
+              <div className="col-md-4 col-12" key={idx}>
+                <div className="card mb-3">
+                  <h3 className="card-header">{data.name}</h3>
+                  <img className="cardImg" src={`../static/images/${photo}`} alt="Card image" />
+                  <div className="card-body">
+                    <p className="card-text">{data.title}</p>
+                  </div>
+                </div>
+              </div>
+            )
+          })}
+        </div>
+        <div className="row">
+          <ul className="result">
+            <li>隊名：McLaren</li>
+            <li>引擎：Mercedes</li>
+            <li>車型：MP4-15</li>
+            <li>年份：2000</li>
+            <li>勝場：7</li>
+            <li>桿位：7</li>
+            <li>最速圈：12</li>
+            <li>年度排名：2nd</li>
+            <li>車隊經理：Ron Dennis</li>
+            <li>設計師：Adrian Newey</li>
+          </ul>
+        </div>
+      </div>
+      <div className="footer bg-primary">
+        &copy; Johnny 0983720128 <br />nintendof1@gmail.com <br />https://github.com/capeta0507?tab=repositories
       </div>
     </div>
-
-    <style jsx>{`
-      .hero {
-        width: 100%;
-        color: #333;
-      }
-      .title {
-        margin: 0;
-        width: 100%;
-        padding-top: 80px;
-        line-height: 1.15;
-        font-size: 48px;
-      }
-      .title,
-      .description {
-        text-align: center;
-      }
-      .row {
-        max-width: 880px;
-        margin: 80px auto 40px;
-        display: flex;
-        flex-direction: row;
-        justify-content: space-around;
-      }
-      .card {
-        padding: 18px 18px 24px;
-        width: 220px;
-        text-align: left;
-        text-decoration: none;
-        color: #434343;
-        border: 1px solid #9b9b9b;
-      }
-      .card:hover {
-        border-color: #067df7;
-      }
-      .card h3 {
-        margin: 0;
-        color: #067df7;
-        font-size: 18px;
-      }
-      .card p {
-        margin: 0;
-        padding: 12px 0 0;
-        font-size: 13px;
-        color: #333;
-      }
-    `}</style>
-  </div>
-)
+  )
+}
 
 export default Home
