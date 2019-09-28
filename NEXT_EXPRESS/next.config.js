44d4a0db6374a42d8b64561662812de4b0e00fb8
capeta0507 <nintendof1@gmail.com>
2019-09-28 16:37:26 +0800

    next_config.js

--- NEXT_EXPRESS/next.config.js
+++ NEXT_EXPRESS/next.config.js
@@ -3,7 +3,7 @@
 // 增加 ./ root 路徑 (提通 npm run export 建立靜態網頁，並且 ./ 為跟目錄)
 const withCSS = require('@zeit/next-css');
 module.exports =  withCSS({
-  cssModules: false,
+  // cssModules: false,
   assetPrefix: './'
 })
 
